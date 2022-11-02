3c2336ec71112c77a7407ff2a155dd29fad8f051
Otchy <id+github@otchy.net>
2022-11-02 19:02:54 +0900

    0.0.1: Make the extension works with minimal setup

--- bin/prepare.mjs
+++ bin/prepare.mjs
@@ -1,15 +1,19 @@
 import fs from 'fs';
 
+// manifest.json
 const packageJsonPath = './package.json';
 const srcPath = './src/manifest.json';
 const destPath = './build/manifest.json';
-
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-
 const destJson = JSON.parse(fs.readFileSync(srcPath));
-
 ['description', 'version'].forEach((field) => {
     destJson[field] = packageJson[field];
 });
-
 fs.writeFileSync(destPath, JSON.stringify(destJson, null, 4));
+
+// html files
+['popup'].forEach((fileName) => {
+    const srcPath = `./src/${fileName}.html`;
+    const destPath = `./build/${fileName}.html`;
+    fs.copyFileSync(srcPath, destPath);
+});
