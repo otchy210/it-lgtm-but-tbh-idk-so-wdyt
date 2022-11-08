e4c39d4ec08bc5fdc3a27f14e48c8c7cd050b8b6
Otchy <id+github@otchy.net>
2022-11-09 00:13:58 +0900

    0.0.4: Get text node under cursor

--- src/page.ts
+++ src/page.ts
@@ -1,5 +1,37 @@
-import { common } from './common';
+import { TextNodeRange } from './utils/TextNodeRange';
 
-console.log('page.ts');
+const isTextNode = (elem: Element | Text): elem is Text => {
+    return elem.nodeType == Node.TEXT_NODE;
+};
 
-common();
+const getTextNodeFromPoint = (elem: Element | Text, x: number, y: number): TextNodeRange | undefined => {
+    if (isTextNode(elem)) {
+        const range = TextNodeRange.of(elem);
+        if (range.contains(x, y)) {
+            return range;
+        }
+    } else {
+        for (const child of elem.childNodes) {
+            const range = getTextNodeFromPoint(child as Element, x, y);
+            if (range) {
+                return range;
+            }
+        }
+    }
+};
+const tryShowingCard = (e: MouseEvent) => {
+    const { target, x, y } = e;
+    const elem = target as Element;
+    const range = getTextNodeFromPoint(elem, x, y);
+    console.log(range);
+};
+
+let timeoutId;
+const onMouseMove = (e: MouseEvent) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        tryShowingCard(e);
+    }, 200);
+};
+
+document.body.addEventListener('mousemove', onMouseMove);
---
+++ src/utils/TextNodeRange.ts
@@ -0,0 +1,55 @@
+// import { Highlighter, useHighlighter } from '../page/Highlighter';
+// import { getPossibleTimeZoneIshWords, getPossibleTimeOffsetWords, PossibleWordsIndexes } from './Texts';
+
+export class TextNodeRange {
+    private node: Text;
+    readonly text: string;
+    private range: Range;
+    // private highlighter: Highlighter;
+
+    private constructor(node: Text) {
+        const range = node.ownerDocument.createRange();
+        range.selectNodeContents(node);
+        this.node = node;
+        this.text = this.node.textContent;
+        this.range = range;
+        // this.highlighter = useHighlighter();
+    }
+    contains(x: number, y: number): boolean {
+        const rect = this.range.getBoundingClientRect();
+        return rect.left <= x && x <= rect.right && rect.top <= y && y <= rect.bottom;
+    }
+    // hightlight(): TextNodeRange {
+    //     this.highlighter.highlight(this.range);
+    //     return this;
+    // }
+    start(start: number): TextNodeRange {
+        this.range.setStart(this.node, start);
+        return this;
+    }
+    end(end: number): TextNodeRange {
+        this.range.setEnd(this.node, end);
+        return this;
+    }
+    // private wordsContains(text: string, words: PossibleWordsIndexes, x: number, y: number): [start: number, end: number, word: string] | undefined {
+    //     for (const word of words) {
+    //         const [start, end] = word;
+    //         if (this.start(start).end(end).contains(x, y)) {
+    //             return [start, end, text.slice(start, end)];
+    //         }
+    //     }
+    // }
+    // findwordUnder(x: number, y: number): [start: number, end: number, word: string] | undefined {
+    //     const possibleTimeZoneIshWords = getPossibleTimeZoneIshWords(this.text);
+    //     const possibleTimeZoneIshWord = this.wordsContains(this.text, possibleTimeZoneIshWords, x, y);
+    //     if (possibleTimeZoneIshWord) {
+    //         return possibleTimeZoneIshWord;
+    //     }
+    //     const timeOffsetWords = getPossibleTimeOffsetWords(this.text);
+    //     return this.wordsContains(this.text, timeOffsetWords, x, y);
+    // }
+
+    static of(node: Text) {
+        return new TextNodeRange(node);
+    }
+}
