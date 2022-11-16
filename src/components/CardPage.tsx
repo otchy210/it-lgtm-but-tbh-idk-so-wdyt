69bbcc0827405b0cf771fbf08a194f54d0e9ad28
Otchy <id+github@otchy.net>
2022-11-17 06:51:24 +0900

    0.0.12: Design card.html

--- src/components/CardPage.tsx
+++ src/components/CardPage.tsx
@@ -1,9 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 import { useBackgroundApi } from '../message/BackgroundApi';
+import GlobalStyle from './GlobalStyle';
+
+const blue = '#0a6670';
+const white = '#fff';
+const lightGrey = '#ccc';
+
+const FlexBox = styled.div`
+    display: flex;
+`;
+
+const CardOuter = styled.div`
+    background-color: ${white};
+    padding: 1px;
+    border-radius: 6px;
+`;
+
+const Card = styled.div`
+    border: solid 3px ${blue};
+    border-radius: 6px;
+    background-color: ${white};
+`;
+
+const Title = styled.div`
+    color: ${white};
+    font-weight: bold;
+    background-color: ${blue};
+    padding: 4px 8px 6px 6px;
+    line-height: 1;
+    border-radius: 0 0 6px 0;
+`;
+
+const Phrase = styled.div`
+    padding: 4px 6px;
+    line-height: 1;
+    border-bottom: solid 1px ${lightGrey};
+    &:last-child {
+        border-bottom: none;
+    }
+`;
 
 const CardPage: React.FC = () => {
     const url = new URL(location.href);
     const word = url.searchParams.get('word');
+    const outerRef = useRef<HTMLDivElement>();
     const [phrases, setPhrases] = useState<[string]>();
     const api = useBackgroundApi();
 
@@ -11,10 +52,12 @@ const CardPage: React.FC = () => {
         api.getPhrases(word).then(setPhrases);
     }, [word]);
     useEffect(() => {
-        if (!phrases) {
+        if (!phrases || !outerRef.current) {
             return;
         }
-        // notify the card loaded
+        const rect = outerRef.current.getBoundingClientRect();
+        console.log(rect);
+        // notify rect.width and rect.height
     }, [phrases]);
 
     if (!phrases) {
@@ -22,12 +65,21 @@ const CardPage: React.FC = () => {
     }
 
     return (
-        <div>
-            <div>{word}</div>
-            {phrases.map((phrase) => {
-                return <div>{phrase}</div>;
-            })}
-        </div>
+        <>
+            <GlobalStyle />
+            <FlexBox>
+                <CardOuter ref={outerRef}>
+                    <Card>
+                        <FlexBox>
+                            <Title>{word}</Title>
+                        </FlexBox>
+                        {phrases.map((phrase) => {
+                            return <Phrase>{phrase}</Phrase>;
+                        })}
+                    </Card>
+                </CardOuter>
+            </FlexBox>
+        </>
     );
 };
 
---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    font-size: 10pt;
+    font-family: sans-serif;
+}`;
+
+export default GlobalStyle;
