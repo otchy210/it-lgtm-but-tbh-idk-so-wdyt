fd085ddc360bf8ea7717556c82044723580c2fe6
Otchy <id+github@otchy.net>
2022-11-28 14:24:56 +0900

    0.0.27: Make /.; considered as a part of word

--- src/utils/Texts.ts
+++ src/utils/Texts.ts
@@ -2,30 +2,35 @@ const lowerA = 'a'.codePointAt(0);
 const lowerZ = 'z'.codePointAt(0);
 const upperA = 'A'.codePointAt(0);
 const upperZ = 'Z'.codePointAt(0);
-const number0 = '0'.codePointAt(0);
-const number9 = '9'.codePointAt(0);
+const digit0 = '0'.codePointAt(0);
+const digit9 = '9'.codePointAt(0);
+const availableSigns = ['/', '.', ';'].map((c) => c.codePointAt(0));
 
-export const isAlphabet = (code: number): boolean => {
+const isAlphabet = (code: number): boolean => {
     return (lowerA <= code && code <= lowerZ) || (upperA <= code && code <= upperZ);
 };
 
-export const isDigit = (code: number): boolean => {
-    return number0 <= code && code <= number9;
+const isDigit = (code: number): boolean => {
+    return digit0 <= code && code <= digit9;
 };
 
-export const isAlphabetOrDigit = (code: number): boolean => {
-    return isAlphabet(code) || isDigit(code);
+const isAvailableSign = (code: number): boolean => {
+    return availableSigns.includes(code);
+};
+
+const isAvaiableChar = (code: number): boolean => {
+    return isAlphabet(code) || isDigit(code) || isAvailableSign(code);
 };
 
 export type PossibleWordsIndexes = [index: number, end: number][];
 
-const getWords = (text: string, isCharInWord: (code: number) => boolean): PossibleWordsIndexes => {
+export const getPossiblehWords = (text: string): PossibleWordsIndexes => {
     const results: [index: number, length: number][] = [];
     let inWord = false;
     let start = -1;
     for (let i = 0; i < text.length; i++) {
         const code = text.charCodeAt(i);
-        if (isCharInWord(code)) {
+        if (isAvaiableChar(code)) {
             if (!inWord) {
                 start = i;
             }
@@ -42,8 +47,4 @@ const getWords = (text: string, isCharInWord: (code: number) => boolean): Possib
         results.push([start, text.length]);
     }
     return results;
-};
-
-export const getPossiblehWords = (text: string): PossibleWordsIndexes => {
-    return getWords(text, isAlphabetOrDigit);
 };
